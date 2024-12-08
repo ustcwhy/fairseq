7e14d23096936f312701c8162e3475f604c6e01a
ustcwhy <wanghongyu22@mails.ucas.ac.cn>
2024-12-08 16:30:53 +0000

    warn: remove check on token_len for moe

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -795,22 +795,22 @@ class Trainer(object):
         for i, sample in enumerate(samples):  # delayed update loop
             sample, is_dummy_batch = self._prepare_sample(sample)
 
-            # MoE training with --batch-size or --max-sentences set
-            if self.is_moe and getattr(self.cfg.dataset, 'batch_size', None) is not None:
-                try:
-                    fixed_src_seq_length = getattr(self.cfg.task, 'tokens_per_sample', None) or self.cfg.task.max_source_positions
-                    assert sample['vl_laion']['net_input']['src_tokens'].shape[1] == fixed_src_seq_length
-                except:
-                    logger.warning(str(sample['vl_laion'].keys()))
-                    logger.warning(str(sample['vl_laion']['net_input'].keys()))
-                    logger.warning(is_dummy_batch)
-                    logger.warning(
-                        "wrong seq len {} on rank {}".format(
-                                sample['vl_laion']['net_input']['src_tokens'].shape[1],
-                                torch.distributed.get_rank(),
-                        )
-                    )
-                    raise AssertionError
+            # # MoE training with --batch-size or --max-sentences set
+            # if self.is_moe and getattr(self.cfg.dataset, 'batch_size', None) is not None:
+            #     try:
+            #         fixed_src_seq_length = getattr(self.cfg.task, 'tokens_per_sample', None) or self.cfg.task.max_source_positions
+            #         assert sample['vl_laion']['net_input']['src_tokens'].shape[1] == fixed_src_seq_length
+            #     except:
+            #         logger.warning(str(sample['vl_laion'].keys()))
+            #         logger.warning(str(sample['vl_laion']['net_input'].keys()))
+            #         logger.warning(is_dummy_batch)
+            #         logger.warning(
+            #             "wrong seq len {} on rank {}".format(
+            #                     sample['vl_laion']['net_input']['src_tokens'].shape[1],
+            #                     torch.distributed.get_rank(),
+            #             )
+            #         )
+            #         raise AssertionError
 
             def maybe_no_sync():
                 """
